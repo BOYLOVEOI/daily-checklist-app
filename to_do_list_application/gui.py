b8d986cd7fee66ece087b605e1950bf54015b9d5
Paul Lavoie <paullavoie2332@gmail.com>
2023-03-15 14:04:34 -0400

    Add Button Implemented

--- to_do_list_application/gui.py
+++ to_do_list_application/gui.py
@@ -1,12 +1,12 @@
 # Import Statements
-import functions
+import functions 
 import PySimpleGUI as sg 
 
 # Label for the window (HAS TO BE A STRING)
 label = sg.Text("Type in a task")
 
 # InputText() is an input text box... tooltip = tip that appears as user's mouse hovers text box
-input_box = sg.InputText(tooltip = "Enter task")
+input_box = sg.InputText(tooltip = "Enter task", key='-TASK-')
 
 # Creating an event button, to add tasks
 add_button = sg.Button('Add')
@@ -22,8 +22,26 @@ add_button = sg.Button('Add')
 # 2 rows within the window for there are two inner lists. As it is sequentially-ordered, the label row
 # is on top of the input_box row. BUT in [[label, input_box]] there is only one row for there is only one
 # inner list.
-window = sg.Window("Daily Checklist", layout=[[[label],[input_box,add_button]]])
-window.read()
+window = sg.Window("Daily Checklist", 
+                    layout=[[[label],[input_box,add_button]]],
+                    font=('Helvetica', 10))
+   
+while True:
+    # window.read() returns a tuple of the button label and the input box 
+    # (returned as a dict of key, -TASK-, and user-inputted value)   
+    event, values = window.read()
+    print(event, values)
+    match event:
+        # If user presses add
+        case 'Add':
+            to_do_list = functions.get_todos()
+            new_to_do = values['-TASK-'] + '\n' 
+            to_do_list.append(new_to_do)
+            functions.write_todos(to_do_list)
+                    
+        # If user exits the window manually                                                                                                                                                                                  
+        case sg.WINDOW_CLOSED:
+            break
 
 # It is imperative that you close the window through the close() method
-window.close()
+window.close()      
