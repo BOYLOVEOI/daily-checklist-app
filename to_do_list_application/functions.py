51072737ce0ced2846623404b4f2201d4840507e
Paul Lavoie <paullavoie2332@gmail.com>
2023-03-15 13:20:59 -0400

    Remove print(__name__) from functions

--- to_do_list_application/functions.py
+++ to_do_list_application/functions.py
@@ -12,7 +12,7 @@ def write_todos(todo_list_local, text_file=FILE_PATH):
     """Opens a text file and writes onto it the contents of a passed in list"""
     with open(text_file, 'w') as file:
         file.writelines(todo_list_local)
-print(__name__)
+
 # If you want to test specific lines of code and NOT have in executed in your main module (where your
 # importing the module from) you need to have the following if conditional block
 if __name__ == '__main__':
